79dc1b328a334c2912aa5ece0f457139ca70251e
jedsonpcfolha <jedsonpc@hotmail.com>
2026-07-29 00:35:18 -0300

    Refine transparent centered login

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ const navGroups = [
       items: [["IA", "Importar Access"]],
     },
   ],
-  SYSTEM_VERSION = "1.3.49",
+  SYSTEM_VERSION = "1.3.50",
   LAST_UPDATE = "29/07/2026";
 type Company = { sourceId: number; name: string };
 type LocalUser = {
